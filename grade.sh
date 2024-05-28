d6e266b6bf0d4bd5f71c95a368af9ec9f3a80444
Alex Zheng <a9zheng@ucsd.edu>
2024-05-28 16:10:29 -0700

    tail -n 2 output.txt > "test_result.txt"

--- grade.sh
+++ grade.sh
@@ -12,5 +12,28 @@ echo 'Finished cloning'
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
+
+if [[ -f "student-submission/ListExamples.java" ]]
+then 
+    cp -r "student-submission/ListExamples.java" "TestListExamples.java" "grading-area/"
+    cp -r "lib/" "grading-area/"
+    cd "grading-area/"
+    javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+    if [[ $? == 0 ]]
+    then
+        echo "Compiled" 
+    else
+        echo "Did not compile correctly."
+    fi
+    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > "output.txt"
+    grep -i "Tests run" output.txt > test_result.txt
+    tests_ran=`cat test_result.txt | cut -d " " -f 3 | cut -d "," -f 1`
+    tests_failed=`cat test_result.txt | cut -d " " -f 6`
+    test_passed=$((tests_ran - tests_failed))
+    echo "Test passed: $test_passed, Tests failed: $tests_failed"
+    exit
+else
+    echo "Missing File"
+fi
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
